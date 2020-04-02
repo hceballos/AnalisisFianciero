880c0dee49c60519f598369d7d17f7808c4e032c
Hector Ceballos Avilés <35620898+hceballos@users.noreply.github.com>
2020-04-02 20:50:02 -0300

    si existe base de dato anterior, remover.

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
-from xls2db import xls2db
+# https://packaging.python.org/tutorials/packaging-projects/
 from lib.spreadsheet import Spreadsheet
+from os import path
+from os import remove
 
 excel_path_Devengo  = 'input/Cartera_Financiera_Presupuestaria_Devengo/*/*'
 excel_path_Contable = 'input/Cartera_Financiera_Contable/*/*'
+
+if path.exists("save_pandas.db"):
+    remove('save_pandas.db')
 
 if __name__ == '__main__':
 
